46156b7114951e3443c3775d207a0cac7f426c29
Lucky-L1zard <sansangol777@gmail.com>
2026-03-07 17:54:31 +0200

    Added Main and first "raw" versions of Models

---
+++ Lab1/src/main/java/Models/Discipline.java
@@ -0,0 +1,13 @@
+package Models;
+
+public class Discipline {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Lab1/src/main/java/Models/Student.java
@@ -0,0 +1,26 @@
+package Models;
+
+public class Student {
+    private String firstName;
+    private String secondName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getFullName() {
+        return this.getFirstName() + " " + this.getSecondName();
+    }
+}
